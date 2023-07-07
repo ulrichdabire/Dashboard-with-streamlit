e9561197f8a319f8bc6897285b76463bbaf523de
ulrichdabire <109689009+ulrichdabire@users.noreply.github.com>
2023-07-07 11:19:25 +0000

    Add files via upload

---
+++ API.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import pandas as pd
+
+app = FastAPI()
+
+# Charger les données et les fusionner
+clics = pd.read_csv("./clics.csv")
+impressions = pd.read_csv("./impressions.csv")
+achats = pd.read_csv("./achats.csv")
+
+merged_data = pd.merge(clics, impressions, on="cookie_id")
+merged_data = pd.merge(merged_data, achats, on="cookie_id")
+
+
+@app.get("/dabireapi/data")
+async def get_data():
+    return JSONResponse(content=merged_data.to_dict(orient="records")) # Retourne les données au format JSON
+
+
